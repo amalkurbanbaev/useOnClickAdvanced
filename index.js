34adda4a7879b0d9283470b55344ceb85dc34146
Amal' Kurbanbaev <amalkurbanbaev@yandex.ru>
2022-02-11 20:24:28 +0500

    Init commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+
+const useOnClickOutside = (refs, handler) => {
+  // create array if node was deleted or not assigned
+  const prettyArray = refs.filter((ref) => {
+    if (ref.current !== null && ref.current !== undefined) {
+      return ref;
+    }
+    return 0;
+  });
+
+  useEffect(() => {
+    const listener = (event) => {
+      // check array elements and do nothing if clicking ref's element or descendent elements
+      for (let i = 0; i < prettyArray.length; i += 1) {
+        if (
+          prettyArray[i] === null ||
+          !prettyArray[i].current ||
+          prettyArray[i].current.contains(event.target)
+        ) {
+          return;
+        }
+      }
+      // do, if clicked on area excluded in condition above
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [refs, handler, prettyArray]);
+};
+
+export default useOnClickOutside;
